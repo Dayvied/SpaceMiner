3e63dbfa2448b774e24f5a6ce7ac13f396bc9a26
David Teu <david95teu@gmail.com>
2023-02-01 16:47:31 +0800

    dfhdf

--- app/src/main/java/com/lesson/spaceminer/pager/CommonPagerAdapter.java
+++ app/src/main/java/com/lesson/spaceminer/pager/CommonPagerAdapter.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class CommonPagerAdapter extends FragmentPagerAdapter {
 
-    private List<Fragment> fragmentList = new ArrayList<>();//99
+    private List<Fragment> fragmentList = new ArrayList<>();//
 
     public CommonPagerAdapter(FragmentManager fm) {
         super(fm);
